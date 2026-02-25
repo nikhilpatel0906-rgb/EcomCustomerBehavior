3fbc14f09c40659174076314ccbaa84c30fc8f03
nikhilpatel0906-rgb <nikhilpatel0906@gmail.com>
2026-02-25 14:59:37 +0530

    Update FetchData.sql
    Added all SQL for necessary data required for analysis

--- FetchData.sql
+++ FetchData.sql
@@ -1,8 +1,8 @@
 -- This file have SQL for needed Data
 
--- Tabel Name : online_shoppers
+-- Table Name : online_shoppers
 
--- Tabel Col: 
+-- Table Col: 
 -- Administrative
 -- Administrative_Duration
 -- Informational
@@ -21,3 +21,107 @@
 -- VisitorType
 -- Weekend
 -- Revenue
+
+
+-- Total Sessions & Purchase Sessions
+
+SELECT COUNT(*) AS total_sessions,
+    SUM(CASE WHEN Revenue = 'TRUE' THEN 1 ELSE 0 END) AS total_purchases
+FROM online_shoppers;
+
+
+-- Overall Conversion Rate (VERY IMPORTANT)
+
+SELECT 
+    ROUND(
+        SUM(CASE WHEN Revenue='TRUE' THEN 1 ELSE 0 END) * 100.0 / COUNT(*),
+        2
+    ) AS conversion_rate_percent
+FROM online_shoppers;
+
+
+-- Returning vs New Visitor Conversion
+
+SELECT
+    VisitorType,
+    COUNT(*) AS total_sessions,
+    SUM(CASE WHEN Revenue='TRUE' THEN 1 ELSE 0 END) AS purchases,
+    ROUND(SUM(CASE WHEN Revenue='TRUE' THEN 1 ELSE 0 END)*100.0/COUNT(*),2) AS conversion_rate
+FROM online_shoppers
+GROUP BY VisitorType;
+
+
+-- Weekend vs Weekday Conversion
+
+SELECT
+    Weekend,
+    COUNT(*) AS total_sessions,
+    SUM(CASE WHEN Revenue='TRUE' THEN 1 ELSE 0 END) AS purchases,
+    ROUND(SUM(CASE WHEN Revenue='TRUE' THEN 1 ELSE 0 END)*100.0/COUNT(*),2) AS conversion_rate
+FROM online_shoppers
+GROUP BY Weekend;
+
+-- Monthly Purchase Trend
+
+SELECT
+    Month,
+    COUNT(*) AS total_sessions,
+    SUM(CASE WHEN Revenue='TRUE' THEN 1 ELSE 0 END) AS purchases
+FROM online_shoppers
+GROUP BY Month
+ORDER BY purchases DESC;
+
+
+-- Bounce Rate Comparison 
+
+SELECT
+    Revenue,
+    ROUND(AVG(BounceRates),4) AS avg_bounce_rate
+FROM online_shoppers
+GROUP BY Revenue;
+
+
+-- Exit Rate Comparison
+
+SELECT
+    Revenue,
+    ROUND(AVG(ExitRates),4) AS avg_exit_rate
+FROM online_shoppers
+GROUP BY Revenue;
+
+
+-- Engagement Analysis
+
+SELECT
+    Revenue,
+    ROUND(AVG(ProductRelated_Duration),2) AS avg_product_time
+FROM online_shoppers
+GROUP BY Revenue;
+
+
+-- Best Traffic Source
+
+SELECT
+    TrafficType,
+    COUNT(*) AS sessions,
+    SUM(CASE WHEN Revenue='TRUE' THEN 1 ELSE 0 END) AS purchases,
+    ROUND(SUM(CASE WHEN Revenue='TRUE' THEN 1 ELSE 0 END)*100.0/COUNT(*),2) AS conversion_rate
+FROM online_shoppers
+GROUP BY TrafficType
+ORDER BY conversion_rate DESC;
+
+
+-- Top Browsers Used by Buyers
+
+
+SELECT
+    Browser,
+    COUNT(*) AS buyer_sessions
+FROM online_shoppers
+WHERE Revenue='TRUE'
+GROUP BY Browser
+ORDER BY buyer_sessions DESC;
+
+
+
+
